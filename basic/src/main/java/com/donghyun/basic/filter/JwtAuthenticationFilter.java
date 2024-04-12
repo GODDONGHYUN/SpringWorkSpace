d5e2b3d8ab2788737398bdd0d3bbb092ef681465
GODDONGHYUN <leenl1168@gmail.com>
2024-04-12 16:50:13 +0900

    2024-04-12 Spring(Spring Security 개념 및 사용)

---
+++ basic/src/main/java/com/donghyun/basic/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,9 @@
+package com.donghyun.basic.filter;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+// # OncePerRequestFilter :
+// * 해당 클래스를 필터 클래스로 지정하는 추상 클래스
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+}
